c376abda416cebd459529a9c5372f66c5d9c7c51
Gaurav Gahlot <gaurav.gahlot19@gmail.com>
2020-05-19 22:39:07 +0530

    updated workflows to use cache
    Signed-off-by: Gaurav Gahlot <gaurav.gahlot19@gmail.com>

--- src/client/workflow.go
+++ src/client/workflow.go
@@ -2,11 +2,13 @@ package client
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"io"
 	"strconv"
 	"time"
 
+	"github.com/gauravgahlot/tink-wizard/src/pkg/redis"
 	"github.com/gauravgahlot/tink-wizard/src/pkg/types"
 	log "github.com/sirupsen/logrus"
 	"github.com/tinkerbell/tink/protos/workflow"
@@ -15,6 +17,24 @@ import (
 
 // ListWorkflows returns a list of workflows
 func ListWorkflows(ctx context.Context) ([]types.Workflow, error) {
+	wfs, err := cache.GetAll(redis.CacheKeys.Workflows)
+	if err != nil || wfs == nil || len(wfs) == 0 {
+		return listWorkflowsFromServer(ctx)
+	}
+	workflows := []types.Workflow{}
+	for id, wf := range wfs {
+		var w types.Workflow
+		if err := json.Unmarshal([]byte(wf), &w); err != nil {
+			log.Error(err)
+			cache.Delete(redis.CacheKeys.Workflows, id)
+			continue
+		}
+		workflows = append(workflows, w)
+	}
+	return workflows, nil
+}
+
+func listWorkflowsFromServer(ctx context.Context) ([]types.Workflow, error) {
 	res, err := workflowClient.ListWorkflows(ctx, &workflow.Empty{})
 	if err != nil {
 		return nil, err
@@ -23,9 +43,14 @@ func ListWorkflows(ctx context.Context) ([]types.Workflow, error) {
 	var wf *workflow.Workflow
 	err = nil
 	for wf, err = res.Recv(); err == nil && wf.Template != ""; wf, err = res.Recv() {
-		w, _ := GetWorkflow(ctx, wf.GetId(), false)
-		w.CreatedAt = time.Unix(wf.GetCreatedAt().Seconds, 0).Local().Format(time.UnixDate)
-		workflows = append(workflows, w)
+		w, err := getWorkflow(ctx, wf.GetId())
+		if err == nil {
+			w.CreatedAt = time.Unix(wf.GetCreatedAt().Seconds, 0).Local().Format(time.UnixDate)
+			if err := cache.Set(redis.CacheKeys.Workflows, w.ID, w); err != nil {
+				log.Error(err)
+			}
+			workflows = append(workflows, w)
+		}
 	}
 	if err != nil && err != io.EOF {
 		log.Fatal(err)
@@ -33,8 +58,7 @@ func ListWorkflows(ctx context.Context) ([]types.Workflow, error) {
 	return workflows, nil
 }
 
-// GetWorkflow returns details for the requested workflow ID
-func GetWorkflow(ctx context.Context, id string, fillDetails bool) (types.Workflow, error) {
+func getWorkflow(ctx context.Context, id string) (types.Workflow, error) {
 	w, err := workflowClient.GetWorkflow(ctx, &workflow.GetRequest{Id: id})
 	if err != nil {
 		return types.Workflow{}, err
@@ -43,22 +67,46 @@ func GetWorkflow(ctx context.Context, id string, fillDetails bool) (types.Workfl
 		return types.Workflow{}, fmt.Errorf("no data found for workflow ID: %v", id)
 	}
 	wf := types.Workflow{
-		ID:       id,
-		RawData:  w.GetData(),
-		Template: templateNames[w.GetTemplate()],
-		State:    w.GetState().String(),
+		ID:      id,
+		RawData: w.GetData(),
+		State:   w.GetState().String(),
 	}
-	details := *parseWorkflowYAML(w.GetData())
+	t, _ := cache.Get(redis.CacheKeys.TemplateNames, w.GetTemplate())
+	json.Unmarshal([]byte(t), &wf.Template)
+	setNameAndTimeout(&wf)
+	return wf, nil
+}
+
+func setNameAndTimeout(wf *types.Workflow) {
+	details := *parseWorkflowYAML(wf.RawData)
 	wf.Name = details.Name
 	wf.Timeout = strconv.Itoa(details.GlobalTimeout)
+}
+
+// GetWorkflow returns details for the requested workflow ID
+func GetWorkflow(ctx context.Context, id string, fillDetails bool) (types.Workflow, error) {
+	result, err := cache.Get(redis.CacheKeys.Workflows, id)
+	if err != nil || result == "" {
+		wf, err := getWorkflow(ctx, id)
+		if err != nil {
+			return types.Workflow{}, err
+		}
+		if fillDetails {
+			wf.Details = *parseWorkflowYAML(wf.RawData)
+		}
+		return wf, nil
+	}
+
+	var wf types.Workflow
+	json.Unmarshal([]byte(result), &wf)
 	if fillDetails {
-		wf.Details = details
+		wf.Details = *parseWorkflowYAML(wf.RawData)
 	}
 	return wf, nil
 }
 
 func parseWorkflowYAML(data string) *types.WorkflowDetails {
-	var wf = types.WorkflowDetails{}
+	var wf types.WorkflowDetails
 	yaml.UnmarshalStrict([]byte(data), &wf)
 	return &wf
 }
