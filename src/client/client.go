414ea85dae5b2df52711b70a17f36b8feeffcd21
stolsma <github@tolsma.net>
2021-01-07 14:02:25 +0100

    Get the Client gRPC side working again
    This commit only solves all the bugs created by the changing Tink gRPC
    client API. A follow up commit will optimize some code paths.
    
    Signed-off-by: stolsma <github@tolsma.net>

--- src/client/client.go
+++ src/client/client.go
@@ -21,9 +21,9 @@ import (
 var (
 	cache *redis.Cache
 
-	templateClient template.TemplateClient
+	templateClient template.TemplateServiceClient
 	hardwareClient hardware.HardwareServiceClient
-	workflowClient workflow.WorkflowSvcClient
+	workflowClient workflow.WorkflowServiceClient
 )
 
 // Init initializes a gRPC connection with server
@@ -32,9 +32,9 @@ func Init() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	templateClient = template.NewTemplateClient(conn)
+	templateClient = template.NewTemplateServiceClient(conn)
 	hardwareClient = hardware.NewHardwareServiceClient(conn)
-	workflowClient = workflow.NewWorkflowSvcClient(conn)
+	workflowClient = workflow.NewWorkflowServiceClient(conn)
 }
 
 // GetConnection returns a gRPC client connection
